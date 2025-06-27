58b4a8baf860810c8122cd22732846e06a6fd293
Ali Hussein <ah0979075074@gmail.com>
2025-06-27 08:53:03 +0300

    set debug =true in settings.py

--- Lost_and_found/Lost_and_found/settings.py
+++ Lost_and_found/Lost_and_found/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f5)=xc!fh+$(@+%hn%^-enm-12$5q8w)a2se)b!#u7l87^2o-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
